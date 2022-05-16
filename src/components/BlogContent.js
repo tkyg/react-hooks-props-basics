e9630f518f1029a81fab1c31cdba13e321cf0df4
tkyg <80114433+tkyg@users.noreply.github.com>
2022-05-16 13:16:24 -0400

    completed

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  return (
+  <div id="blog-content">
+    <h1>{props.articleText} {props.addText}</h1>
+    <p>{props.minutesToRead}</p>
+  </div>
+  )
 }
 
 export default BlogContent;
--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,7 +5,10 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." 
+      additionalText="I dont know how true that is"
+      minutesToRead={1}/>
+      <BlogContent addText="This is my personal opinion."/>
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
